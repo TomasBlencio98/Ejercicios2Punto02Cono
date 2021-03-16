ea84782d1c93d9b6a57c0a7a216625c52fbbed42
HP <HP@DESKTOP-2MTUV5R>
2021-03-15 21:15:19 -0300

    Programa del cono completo

---
+++ Ejercicios2Punto02Cono.BL/Cono.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Ejercicios2Punto02Cono.BL
+    
+{
+    public class Cono
+    {
+        //Atributos
+        public double Generatriz { get; set; }
+        public double Altura { get; set; }
+        public double Radio { get; set; }
+
+        public Cono()
+        {
+              
+        }
+
+        public Cono(double generatriz, double altura, double radio)
+        {
+            Generatriz = generatriz;
+            Altura = altura;
+            Radio = radio;
+        }
+
+        //Métodos
+
+        public double GetArea()
+        {
+            return Math.PI * Radio * (Radio + Generatriz);
+        }
+        public double GetVolumen()
+        {
+            return (Math.PI * Math.Pow(Radio, 2) * Altura) / 3;
+        }
+    }
+}
---
+++ Ejercicios2Punto02Cono.Consola/Program.cs
@@ -0,0 +1,47 @@
+﻿using Ejercicios2Punto02Cono.BL;
+using System;
+
+namespace Ejercicios2Punto02Cono.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cono cono = new Cono();
+            Console.WriteLine("Se informara el Area y Volumen de un Cono");
+            Console.Write("Por favor ingrese la Generatriz del cono:  ");
+            if (double.TryParse(Console.ReadLine(), out var generatriz))
+            {
+                cono.Generatriz = generatriz;
+                Console.Write("Por favor ingrese la Altura del cono:  ");
+                if (double.TryParse(Console.ReadLine(), out var altura))
+                {
+                    cono.Altura = altura;
+                    Console.Write("Por favor ingrese el Radio de giro del cono:   ");
+                    if (double.TryParse(Console.ReadLine(), out var radio))
+                    {
+                        cono.Radio = radio;
+                        Console.WriteLine("");
+                        Console.WriteLine("Se informara los datos del cono");
+                        Console.WriteLine($"Generatriz:{generatriz} Altura:{altura} ; Radio:{radio}");
+                        Console.WriteLine($"Area:{cono.GetArea():N2}");
+                        Console.WriteLine($"Volumen:{cono.GetVolumen():N2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Radio de giro no valido");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Altura ingresada no valida");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Generatriz ingresada no valida");
+            }
+            Console.ReadLine();
+        }
+    }
+}
